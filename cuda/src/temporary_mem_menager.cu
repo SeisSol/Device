01726cf14601f85cdb771cdae189ef3d1c0f9a36
ravil <ravil.dorozhinskii@tum.de>
2020-02-13 16:31:58 +0100

    Added an env. var. to control the anmount of temp. mem

--- cuda/src/temporary_mem_menager.cu
+++ cuda/src/temporary_mem_menager.cu
@@ -1,10 +1,12 @@
 #include <assert.h>
+#include <iostream>
+#include <string>
 
 #include "device_utils.h"
 #include "temporary_mem_menager.h"
 
 // default size for the tmp mem. = 1GB
-#define MAX_DEFAULT_MEMORY 1024*1024*1024
+#define MAX_DEFAULT_MEMORY 1
 
 
 DeviceTemporaryMemoryMenager::~DeviceTemporaryMemoryMenager() {
@@ -17,7 +19,7 @@ DeviceTemporaryMemoryMenager& DeviceTemporaryMemoryMenager::get_instance() {
 }
 
 byte* DeviceTemporaryMemoryMenager::get_mem(unsigned amount) {
-    assert((m_counter < m_max_mem) && "run of the device temporary mem.");
+    assert((m_counter < m_max_mem) && "run out of the device temporary mem.");
     byte *next_ptr = &m_base_ptr[m_counter];
     m_counter += amount;
     m_stack.push(amount);
@@ -30,8 +32,34 @@ void DeviceTemporaryMemoryMenager::free() {
 }
 
 DeviceTemporaryMemoryMenager::DeviceTemporaryMemoryMenager() : m_base_ptr(nullptr),
-                                                               m_counter(0),
-                                                               m_max_mem(MAX_DEFAULT_MEMORY) {
+                                                               m_counter(0) {
+  long long Factor = 1024 * 1024 * 1024;  //!< bytes in 1 GB
+  m_max_mem = Factor * MAX_DEFAULT_MEMORY; //!< default mem. size
 
-    m_base_ptr = (byte*)device_malloc(m_max_mem);
+  // TODO:(RAVIL) the amount of the temp. must be determined from the analysis done by a source code generator
+  try {
+    char *value_str = std::getenv("DEVICE_STACK_TEMP_MEM_SIZE");
+    if (!value_str) {
+      std::cout << "DEVICE::INFO: env. variable \"DEVICE_STACK_TEMP_MEM_SIZE\" has not been set. "
+                << "The default amount of the device memory (" << MAX_DEFAULT_MEMORY << " GB) "
+                << "is going to be used to store temp. variables during execution of compute-kernels\n";
+    }
+    else {
+      double RequestedTempMem = std::stod(std::string(value_str));
+      m_max_mem = Factor * RequestedTempMem;
+      std::cout << "DEVICE::INFO: env. variable \"DEVICE_STACK_TEMP_MEM_SIZE\" has been detected. "
+                << RequestedTempMem << "GB of the device memory is going to be used "
+                << "to store temp. variables during execution of compute-kernels\n";
+    }
+  }
+  catch (const std::invalid_argument &err) {
+    std::cout << "DEVICE::ERROR: " << err.what() << ". File: " << __FILE__ << ", line: " << __LINE__ << "\n";
+    throw err;
+  }
+  catch (const std::out_of_range& err) {
+    std::cout << "DEVICE::ERROR: " << err.what() << ". File: " << __FILE__ << ", line: " << __LINE__ << "\n";
+    throw err;
+  }
+
+  m_base_ptr = (byte*)device_malloc(m_max_mem);
 }
