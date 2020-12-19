27b75bf299da07b448afb689048cfaa9203b6ef8
Ravil Dorozhinskii <ga83dit2@gpu01.srve>
2020-12-19 01:08:35 +0100

    Fixed amd.cmake

--- amd.cmake
+++ amd.cmake
@@ -27,8 +27,10 @@ endif()
 #Only need NVCC at the time no AMD system to deploy to
 set(DEVICE_HIPCC)
 set(DEVICE_HCC)
-if (DEFINED ENV{HIP_PLATFORM} AND $ENV{HIP_PLATFORM} STREQUAL "nvcc")
-    set(DEVICE_NVCC -arch=${DEVICE_SUB_ARCH};-dc;--expt-relaxed-constexpr;-DCUDA_UNDERHOOD)
+if (DEFINED ENV{HIP_PLATFORM})
+    if ($ENV{HIP_PLATFORM} STREQUAL "nvcc")
+        set(DEVICE_NVCC -arch=${DEVICE_SUB_ARCH};-dc;--expt-relaxed-constexpr;-DCUDA_UNDERHOOD)
+    endif()
 endif()
 
 set(DEVICE_SOURCE_FILES device.cpp
