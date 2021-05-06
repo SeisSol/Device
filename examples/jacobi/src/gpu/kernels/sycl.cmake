e8248f9e50668a1704a014046a254236d940edc8
Ludwig Kratzl <kratzl.ludwig@gmail.com>
2021-05-06 08:53:04 +0200

    switch to target

--- examples/jacobi/src/gpu/kernels/sycl.cmake
+++ examples/jacobi/src/gpu/kernels/sycl.cmake
@@ -7,12 +7,13 @@ endif()
 
 set(KERNEL_SOURCE_FILES subroutinesONEAPI.cpp)
 add_library(${TARGET_NAME} SHARED ${KERNEL_SOURCE_FILES})
+target_link_libraries(${TARGET_NAME} PUBLIC stdc++fs)
 
 target_compile_options(${TARGET_NAME} PRIVATE ${EXTRA_FLAGS} -Wall -Wpedantic -std=c++17 -O3)
 target_compile_definitions(${TARGET_NAME} PRIVATE DEVICE_${DEVICE_BACKEND}_LANG REAL_SIZE=${REAL_SIZE_IN_BYTES})
 
 target_include_directories(${TARGET_NAME} PUBLIC root)
-target_link_libraries(${TARGET_NAME} PUBLIC device stdc++fs)
+target_link_libraries(${TARGET_NAME} PUBLIC device)
 
 if (${DEVICE_BACKEND} STREQUAL "HIPSYCL")
     find_package(hipSYCL CONFIG REQUIRED)
