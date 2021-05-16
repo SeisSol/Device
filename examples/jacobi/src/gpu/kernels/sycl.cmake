a21a05cbf54e9f04e4031dce3ab3ef0e73eb4954
Ludwig Kratzl <kratzl.ludwig@gmail.com>
2021-05-16 14:36:40 +0200

    Fix sycl interface due to last changes in SeisSol

--- examples/jacobi/src/gpu/kernels/sycl.cmake
+++ examples/jacobi/src/gpu/kernels/sycl.cmake
@@ -16,7 +16,7 @@ target_include_directories(${TARGET_NAME} PUBLIC root)
 target_link_libraries(${TARGET_NAME} PUBLIC device)
 
 if (${DEVICE_BACKEND} STREQUAL "HIPSYCL")
-    set(HIPSYCL_TARGETS 'cuda:sm_70')
+    set(HIPSYCL_TARGETS 'cuda:sm_${DEVICE_SUB_ARCH}')
     find_package(hipSYCL CONFIG REQUIRED)
     add_sycl_to_target(TARGET ${TARGET_NAME} SOURCES ${DEVICE_SOURCE_FILES})
 else()
