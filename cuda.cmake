7f27b1a5311ee79975c498b75d9b4248157a48b4
ravil-mobile <ravil.mobile.com@gmail.com>
2022-04-25 11:03:13 +0200

    Applied modern cmake to cuda (#15)
    Co-authored-by: ravil <ravil.dorozhinskii@tum.de>

--- cuda.cmake
+++ cuda.cmake
@@ -1,42 +1,44 @@
-find_package(CUDA REQUIRED)
-find_package(NVToolsExt)
-if (NVToolsExt_FOUND)
-    set(EXTRA_DEVICE_FLAGS -DPROFILING_ENABLED)
-endif()
+enable_language(CUDA)
+set(CMAKE_CUDA_STANDARD 14)
 
-set(CUDA_SEPARABLE_COMPILATION ON)
+add_library(device SHARED device.cpp
+                 interfaces/cuda/Control.cu
+                 interfaces/cuda/Aux.cu
+                 interfaces/cuda/Memory.cu
+                 interfaces/cuda/Copy.cu
+                 interfaces/cuda/Streams.cu
+                 interfaces/cuda/Internals.cu
+                 algorithms/cuda/ArrayManip.cu
+                 algorithms/cuda/BatchManip.cu
+                 algorithms/cuda/Debugging.cu
+                 algorithms/cuda/Reduction.cpp)
+
+set_target_properties(device PROPERTIES POSITION_INDEPENDENT_CODE ON)
 set_source_files_properties(device.cpp
                             algorithms/cuda/Reduction.cpp
-                            PROPERTIES CUDA_SOURCE_PROPERTY_FORMAT
-                            OBJ)
+                            PROPERTIES LANGUAGE CUDA)
 
-# NOTE: cmake doesn't fully support $<$<COMPILE_LANGUAGE>:CUDA>:
-set(CUDA_NVCC_FLAGS -std=c++14;
-        -arch=${DEVICE_ARCH};
-        -Xptxas -v;
+
+string(REPLACE "sm_" "" CUDA_DEVICE_ARCH "${DEVICE_ARCH}")
+set_target_properties(device PROPERTIES CUDA_ARCHITECTURES "${CUDA_DEVICE_ARCH}")
+
+target_compile_features(device PRIVATE cxx_std_14)
+
+target_compile_definitions(device PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:
         -DDEVICE_${BACKEND_UPPER_CASE}_LANG;
-        -DREAL_SIZE=${REAL_SIZE_IN_BYTES};
+        -DREAL_SIZE=${REAL_SIZE_IN_BYTES}
+        >)
+
+target_compile_options(device PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:
+        -Xptxas -v;
         --expt-relaxed-constexpr;
-        --compiler-options -fPIC;
-        ${EXTRA_DEVICE_FLAGS})
+        >)
 
-cuda_add_library(device device.cpp
-                        interfaces/cuda/Control.cu
-                        interfaces/cuda/Aux.cu
-                        interfaces/cuda/Memory.cu
-                        interfaces/cuda/Copy.cu
-                        interfaces/cuda/Streams.cu
-                        interfaces/cuda/Internals.cu
-                        algorithms/cuda/ArrayManip.cu
-                        algorithms/cuda/BatchManip.cu
-                        algorithms/cuda/Debugging.cu
-                        algorithms/cuda/Reduction.cpp)
+if (EXTRA_DEVICE_FLAGS)
+    target_compile_options(device PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:
+            ${EXTRA_DEVICE_FLAGS}
+            >)
+endif()
 
-
-target_link_directories(device PUBLIC ${CUDA_TOOLKIT_ROOT_DIR}/lib64/stubs)
-target_link_libraries(device -lcuda ${CUDA_LIBRARIES} ${NVToolsExt_LIBRARIES})
-target_include_directories(device PUBLIC ${CUDA_INCLUDE_DIRS}
-        ${NVToolsExt_INCLUDE_DIRS}
-        ../../src)
-
-target_compile_options(device PRIVATE "-std=c++11")
+find_package(CUDAToolkit REQUIRED)
+target_link_libraries(device PUBLIC CUDA::cudart CUDA::cuda_driver CUDA::nvToolsExt)
