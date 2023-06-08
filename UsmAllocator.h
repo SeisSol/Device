f740ad6f97c7186b52f3dbc327f4de905d1de1e5
ravil <ravil.dorozhinskii@tum.de>
2023-06-08 17:03:51 +0200

    Added an allocator to manage the unified device memory in std

---
+++ UsmAllocator.h
@@ -0,0 +1,61 @@
+#ifndef DEVICE_USM_ALLOCATOR_H
+#define DEVICE_USM_ALLOCATOR_H
+
+#include "device.h"
+#include <cstdlib>
+#include <stdexcept>
+
+namespace device {
+template <typename T>
+class UsmAllocator {
+public:
+  using value_type = T;
+  using pointer = value_type *;
+  using const_pointer = const value_type *;
+  using void_pointer = void *;
+  using const_void_pointer = const void *;
+  using size_type = std::size_t;
+  using difference_type = std::ptrdiff_t;
+
+  UsmAllocator() noexcept = delete;
+  UsmAllocator(device::DeviceInstance& instance) noexcept : api(instance.api) {}
+
+  UsmAllocator(const UsmAllocator &) noexcept = default;
+  UsmAllocator(UsmAllocator &&) noexcept = default;
+
+  UsmAllocator &operator=(const UsmAllocator &) = delete;
+  UsmAllocator &operator=(UsmAllocator &&) = default;
+
+  template <typename U> UsmAllocator(const UsmAllocator<U>& other) noexcept : api(other.api) {}
+
+  pointer allocate(size_type numElements) {
+    auto* ptr = static_cast<pointer>(api->allocUnifiedMem(sizeof(T) * numElements));
+    if (!ptr) {
+      throw std::runtime_error("device::UsmAllocator: Allocation failed");
+    }
+    return ptr;
+  }
+
+  void deallocate(T *ptr, std::size_t) {
+    if (ptr) {
+      api->freeMem(ptr);
+    }
+  }
+
+  template <typename U>
+  friend bool operator==(const UsmAllocator<T> &allocator, const UsmAllocator<U> &otherAllocator) {
+    return allocator.api == otherAllocator.api;
+  }
+
+  template <typename U>
+  friend bool operator!=(const UsmAllocator<T> &allocator, const UsmAllocator<U> &otherAllocator) {
+    return !(allocator == otherAllocator);
+  }
+
+private:
+  template <typename U> friend class UsmAllocator;
+  device::AbstractAPI* api{nullptr};
+};
+} // namespace device
+
+#endif // DEVICE_USM_ALLOCATOR_H
